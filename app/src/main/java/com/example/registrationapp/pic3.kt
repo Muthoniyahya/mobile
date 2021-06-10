28d748295d4ca0634ac2e6444b9a80e21a797e25
Iman_Yahya <Muthoniyahya@gmail.com>
2021-06-10 21:12:03 +0300

    first commit

---
+++ app/src/main/java/com/example/registrationapp/pic3.kt
@@ -0,0 +1,11 @@
+package com.example.registrationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pic3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pic3)
+    }
+}
